7d8c0ee64405c77d6da5d7d04478684af7afe3e3
Karlie-crypto <karlieemoyo@.com>
2023-02-25 13:30:36 -0800

    Factorial numbers

--- factors.c
+++ factors.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
 
-int main()
+int main(void)
 {
-    long long int num = 239809320265259;
-    long int factor1 = 2;
-    long int factor2;
+	long long int num = 239809320265259;
+	long int factor1 = 2;
+	long int factor2;
 
-    while (num % factor1)
-    {
-        if (factor1 <= num)
-        {
-            factor1++;
+	while (num % factor1)
+ 	{
+ 		if (factor1 <= num)
+        {         
+		factor1++;
         }
-        else {
+		else 
+		{
             return (-1);
         }
     }
 
-    factor2 = num / factor1;
-    printf("%lld = %ld * %ld\n", num, factor2, factor1);
-    return (0);
+ 	factor2 = num / factor1;
+	printf("%lld = %ld * %ld\n", num, factor2, factor1);
+	return (0);
 }
